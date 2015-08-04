1a918c18d955dd37b7133eda12b7cbfe34b9fc52
siemdicou <siemdicou@siems-MacBook-Pro.local>
2015-08-04 03:21:30 +0200

    je oude Shizzle

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>aapje</title>
+</head>
+<body>
+<div id="wrapper">
+<img src="http://www.cardea.nl/CmsData/images/Algemeen/2013/jongen-voor-campus-leiderdorp_bw_01-10-2013.jpg">
+</div> 
+</body>
+
+</html>
