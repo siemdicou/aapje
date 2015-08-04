9f5ff120e7a9a4ada17e11b38cb1120013cb6a90
siemdicou <siemdicou@siems-MacBook-Pro.local>
2015-08-04 15:09:23 +0200

    Php Test

---
+++ Fitsupport/Assets/Scripts/Aapje/PhpTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PhpTest : MonoBehaviour {
+	IEnumerator Start(){
+		WWWForm wwwForm = new WWWForm ();
+		wwwForm.AddField ("ValueA", "22");
+		wwwForm.AddField ("ValueB", "44");
+		
+		// Create a download object
+		WWW download = new WWW ("http://localhost/Index.php", wwwForm);
+		// Wait until the download is done
+		yield return download;
+		Debug.Log(download.error);
+		Debug.Log(download.text);
+		
+		string[] values = download.text.Split(new char[] {'\n'});
+		
+		foreach (string value in values)
+		{
+			if (String.IsNullOrEmpty(value.Trim()))
+			{
+				continue;
+			}
+			string[] variable = value.Split (new char[] {'='});
+			
+			Debug.Log ("PHP data: " + variable[0].Trim() + " = " + variable[1].Trim());
+		}
+	}
+}
