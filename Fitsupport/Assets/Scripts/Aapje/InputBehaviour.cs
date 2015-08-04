591698b9e9687e9f5a907ada8c3babe8ef653c93
siemdicou <siemdicou@siems-MacBook-Pro.local>
2015-08-04 14:25:09 +0200

    Aapje

---
+++ Fitsupport/Assets/Scripts/Aapje/InputBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputBehaviour : MonoBehaviour {
+	private bool keyboardOpen = false;
+
+	public void InsertInformation(){
+		if (keyboardOpen == false) {
+			keyboardOpen = true;
+			TouchScreenKeyboard.Open ("", TouchScreenKeyboardType.Default, false, false, true);
+		}
+	}
+}
---
+++ Fitsupport/Assets/Scripts/Aapje/PlatformController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+enum platforms{
+		android,
+		pc
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
