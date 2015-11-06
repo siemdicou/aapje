d42790b5cba53ca9cc408601510b551db0683136
siemdicou <siemdicou@hotmail.com>
2015-11-07 00:18:50 +0100

    hi

--- fotoboek.php
+++ fotoboek.php
@@ -1,11 +1,16 @@
 <?php 
     $result = $mysqli->query("SELECT * FROM fotoboek");
     while($pictures=$result->fetch_assoc()){
-    echo '<div id=pictures>';
-     echo '<img src="'.$pictures['img'].'">';
-    echo '<input type="button" value="kopen" id="koopButton">';
+    echo '<div class=pictures>';
+    echo '<img src="'.$pictures['img'].'">';
+         echo "<div class=textKopen>";
+		 	echo '<h3>'.$pictures['coins'];
+		 	echo '<br><button class="kopen">kopen</button>';
+            echo "</h3>";
+		 echo '</div>';
     echo '</div>';
- 
-
     }
+    // if ($pictures['img']= ) {
+    //     # code...
+    // }
  ?>
